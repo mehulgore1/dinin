68fdb9119a013c3cf6f4e7ad8d83b0ca277d9821
mehulgore <mehulhg@gmail.com>
2020-05-21 15:12:11 -0500

    add firebase to index js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import * as firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyA5mq106o1VmVPrEJef61P2XNNLcFsE2uQ",
+  authDomain: "dinin-2f0b9.firebaseapp.com",
+  databaseURL: "https://dinin-2f0b9.firebaseio.com",
+  projectId: "dinin-2f0b9",
+  storageBucket: "dinin-2f0b9.appspot.com",
+  messagingSenderId: "181153685615",
+  appId: "1:181153685615:web:35cb2d9171341283aa6d9a",
+  measurementId: "G-YSTCM76J2T"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
