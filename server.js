9fcdc4f6be7f482336ec0b508ebb832e05691f74
mehulgore <mehulhg@gmail.com>
2020-08-04 19:20:37 -0700

    heroku mern setup

--- server.js
+++ server.js
@@ -6,15 +6,18 @@ if (process.env.NODE_ENV !== "production") {
 // setup
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 const stripePublishableKey = process.env.STRIPE_PUBLISHABLE_KEY;
 
 const app = express();
+const router = express.Router();
 const port = process.env.PORT || 5000;
 const stripe = require("stripe")(stripeSecretKey);
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("build/"));
 
 //routes
 app.get("/api/pay", (req, res) => {
@@ -54,6 +57,11 @@ app.get("/session_id/:id", async function response(req, res) {
   });
 
   res.send({ checkoutSession });
+});
+
+//serve react if no endpoints hit
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "build/index.html"));
 });
 
 // listener
